aa61d9e68ff7133baf01bc5263431cad93d83a60
Huang Feng <306761352@qq.com>
2013-04-16 10:48:07 +0800

    a

--- index.php
+++ index.php
@@ -11,18 +11,18 @@
  * DEFINE ALL CONSTANTS WE NEED
  *---------------------------------------------------------------
  */
-// 定义文件夹的路径
-define('DS',                                              DIRECTORY_SEPARATOR);
-define('PATH_BASE',                          __DIR__ . DS);
-define('PATH_APP',                             PATH_BASE . 'app' .       DS);
-define('PATH_APP_C',                        PATH_BASE . 'app' .       DS . 'mvc' .        DS . 'controllers' .    DS);
-define('PATH_APP_M',                       PATH_BASE . 'app' .       DS . 'mvc' .        DS . 'models' .          DS);
-define('PATH_ASSETS',                    PATH_BASE . 'assets' .  DS);
-define('PATH_LOGS',                          PATH_BASE . 'logs' .       DS);
-define('PATH_CORE',                         PATH_BASE . 'core' .       DS);
-define('PATH_CORE_LIBS',             PATH_BASE . 'core' .       DS . 'libs' .             DS);
-define('PATH_CORE_HELPERS',  PATH_BASE . 'core' .       DS . 'helpers' .    DS);
-define('PATH_CORE_DEBUG',       PATH_BASE . 'core' .       DS . 'debug' .      DS);
+//                          定义文件夹的路径
+define('DS',                DIRECTORY_SEPARATOR);
+define('PATH_BASE',         __DIR__ . DS);
+define('PATH_APP',          PATH_BASE . 'app' .       DS);
+define('PATH_APP_C',        PATH_BASE . 'app' .       DS . 'mvc' .        DS . 'controllers' .    DS);
+define('PATH_APP_M',        PATH_BASE . 'app' .       DS . 'mvc' .        DS . 'models' .          DS);
+define('PATH_ASSETS',       PATH_BASE . 'assets' .  DS);
+define('PATH_LOGS',         PATH_BASE . 'logs' .       DS);
+define('PATH_CORE',         PATH_BASE . 'core' .       DS);
+define('PATH_CORE_LIBS',    PATH_BASE . 'core' .       DS . 'libs' .             DS);
+define('PATH_CORE_HELPERS', PATH_BASE . 'core' .       DS . 'helpers' .    DS);
+define('PATH_CORE_DEBUG',   PATH_BASE . 'core' .       DS . 'debug' .      DS);
 
 // 定义文件的路径
 define('EXT', '.php');
