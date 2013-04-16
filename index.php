a6e7f307ff34e8cb0d8f4a6df3679d028662c40d
Huang Feng <306761352@qq.com>
2013-04-16 10:36:55 +0800

    a

--- index.php
+++ index.php
@@ -11,28 +11,25 @@
  * DEFINE ALL CONSTANTS WE NEED
  *---------------------------------------------------------------
  */
+// 定义文件夹的路径
+define('DS',                                              DIRECTORY_SEPARATOR);
+define('PATH_BASE',                          __DIR__ . DS);
+define('PATH_APP',                             PATH_BASE . 'app' .       DS);
+define('PATH_APP_C',                        PATH_BASE . 'app' .       DS . 'mvc' .        DS . 'controllers' .    DS);
+define('PATH_APP_M',                       PATH_BASE . 'app' .       DS . 'mvc' .        DS . 'models' .          DS);
+define('PATH_ASSETS',                    PATH_BASE . 'assets' .  DS);
+define('PATH_LOGS',                          PATH_BASE . 'logs' .       DS);
+define('PATH_CORE',                         PATH_BASE . 'core' .       DS);
+define('PATH_CORE_LIBS',             PATH_BASE . 'core' .       DS . 'libs' .             DS);
+define('PATH_CORE_HELPERS',  PATH_BASE . 'core' .       DS . 'helpers' .    DS);
+define('PATH_CORE_DEBUG',       PATH_BASE . 'core' .       DS . 'debug' .      DS);
 
-// define paths of folders
-define('DS',                    DIRECTORY_SEPARATOR);
-define('PATH_BASE',             __DIR__ . DS);
-//var_dump(PATH_BASE); //D:\xampp\htdocs\phx\
-
-define('PATH_APP',              PATH_BASE . 'app' .     DS);
-define('PATH_APP_C',            PATH_BASE . 'app' .     DS . 'mvc' .        DS . 'controllers' .    DS);
-define('PATH_APP_M',            PATH_BASE . 'app' .     DS . 'mvc' .        DS . 'models' .         DS);
-define('PATH_ASSETS',           PATH_BASE . 'assets' .  DS);
-define('PATH_LOGS',             PATH_BASE . 'logs' .    DS);
-define('PATH_CORE',             PATH_BASE . 'core' .    DS);
-define('PATH_CORE_LIBS',        PATH_BASE . 'core' .    DS . 'libs' .       DS);
-define('PATH_CORE_HELPERS',     PATH_BASE . 'core' .    DS . 'helpers' .    DS);
-define('PATH_CORE_DEBUG',       PATH_BASE . 'core' .    DS . 'debug' .      DS);
-
-// define paths of files
+// 定义文件的路径
 define('EXT', '.php');
 define('FILE_LOG_ERRORS',       PATH_LOGS . 'errors.log');
 define('FILE_BASE',             PATH_BASE . 'index' .   EXT);
 
-// define cross platform line break
+// 定义跨平台的行尾结束符
 define('EOL', PHP_EOL);
 
 /*
