db81b665993539734a18381f42455ddc31cc169a
Huang Feng <306761352@qq.com>
2013-05-30 12:50:57 +0800

    milestone

--- app/config/application.php
+++ app/config/application.php
@@ -3,8 +3,8 @@
 return array(
     /* 基本信息，放在一级下 */
 
-    'hostname' => 'localhost',
-    'base_url' => '/phx/',
+    'hostname'           => 'localhost',
+    'base_url'           => '/phx/',
 
     // 时区
     'timezone'           => 'Asia/Shanghai',
@@ -16,23 +16,23 @@ return array(
     'default_action'     => 'index',
 
     // 是否开启错误报告，生产环境中请设置为false
-    'error_reporting' => true,
+    'error_reporting'    => true,
 
     // 是否加载调试工具
-    'debug'         => true,
+    'debug'              => true,
 
     // 调试工具（dbug 或 kint）， 是否再包括firephp
-    'debug_tool'           => 'kint',
+    'debug_tool'         => 'kint',
 
     // 是否开启php-o对象操纵功能
-    'php-o'           => true,
+    'php-o'              => true,
 
     // 加密盐
-    'key'                  => '123456abcdefg',
+    'key'                => '123456abcdefg',
 
     // 这是用来测试的
-    'aaa'                  => array( 'bbb' => '111',
-                                     'ccc' => '222',
-                                     'ddd' => array('eee' => '12345'))
+    'aaa'                => array( 'bbb' => '111',
+                                   'ccc' => '222',
+                                   'ddd' => array('eee' => '12345'))
 
 );
